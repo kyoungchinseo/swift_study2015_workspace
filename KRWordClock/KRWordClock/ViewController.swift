8975a689f3c6fdae13eb33af39ef5c78249321c8
kyoungchin <kcseo.acm@gmail.com>
2015-07-28 00:06:37 +0900

    calculate hour, minute, noon/midnight, am/pm separately without refreshing color

--- KRWordClock/KRWordClock/ViewController.swift
+++ KRWordClock/KRWordClock/ViewController.swift
@@ -55,12 +55,21 @@ class ViewController: NSViewController {
     
     @IBOutlet var second: NSTextField!
     
+    @IBOutlet var times: [NSTextField]!
     
     
     var timer = NSTimer()
     
+    func clear_color() {
+        
+        
+        
+    }
+    
     
     func counting() {
+        clear_color()
+        
         let gregorian = NSCalendar.currentCalendar()
         let dayComponents = gregorian.components([.Hour, .Minute, .Second], fromDate: NSDate())
         
@@ -80,6 +89,111 @@ class ViewController: NSViewController {
             hourPreAmPmPre.textColor = color
             hourPreAmPost.textColor = color
         }
+        
+        print("\(dayComponents.hour%12)")
+        
+        if (dayComponents.hour % 12 == 0) {
+            if (dayComponents.hour / 12 == 1) {
+                // 정오
+                midnightPostMoonPre.textColor = color
+                minuteFiveNoonPost.textColor = color
+            } else {
+                // 자정
+                midnightPre.textColor = color
+                midnightPostMoonPre.textColor = color
+            }
+            
+        } else {
+            switch (dayComponents.hour % 12) {
+            case 1:
+                hourOne.textColor = color
+            case 2:
+                hourTwo.textColor = color
+            case 3:
+                hourThree.textColor = color
+            case 4:
+                hourFour.textColor = color
+            case 5:
+                hourFivePre.textColor = color
+                hourFiveSixPost.textColor = color
+            case 6:
+                hourSixPre.textColor = color
+                hourFiveSixPost.textColor = color
+            case 7:
+                hourSevenPre.textColor = color
+                hourSevenPost.textColor = color
+            case 8:
+                hourEightPre.textColor = color
+                hourEightPost.textColor = color
+            case 9:
+                hourNinePre.textColor = color
+                hourNinePost.textColor = color
+            case 10:
+                hourTen.textColor = color
+            case 11:
+                hourTen.textColor = color
+                hourOne.textColor = color
+            case 12:
+                hourTen.textColor = color
+                hourTwo.textColor = color
+            default:
+                hourPost.textColor = color
+            }
+            hourPost.textColor = color
+            
+        }
+        
+        
+        // minutes
+        //
+        print("\(dayComponents.minute%10)")
+        print("\(dayComponents.minute/10)")
+        
+        switch (dayComponents.minute/10) {
+        case 1:
+            minuteTen.textColor = color
+        case 2:
+            minuteTwenty.textColor = color
+        case 3:
+            minuteThirty.textColor = color
+        case 4:
+            minuteForty.textColor = color
+        case 5:
+            minuteFifty.textColor = color
+        default:
+            minuteTen.textColor = color //
+        }
+        minuteTen.textColor = color
+        
+        switch (dayComponents.minute%10) {
+        case 1:
+            minuteOne.textColor = color
+        case 2:
+            minuteTwo.textColor = color
+        case 3:
+            minuteThree.textColor = color
+        case 4:
+            minuteFour.textColor = color
+        case 5:
+            minuteFiveNoonPost.textColor = color
+        case 6:
+            minuteSix.textColor = color
+        case 7:
+            minuteSeven.textColor = color
+        case 8:
+            minuteEight.textColor = color
+        case 9:
+            minuteNine.textColor = color
+        default:
+            minuteTen.textColor = color //
+        }
+        minutePost.textColor = color
+        
+        
+        // seconds
+        print("\(dayComponents.second%10)")
+        print("\(dayComponents.second/10)")
+        
         
         
     }
